3fd29345508a68a22f42109d7193a9a9178d9715
sumaiak <cph-se160@cphbusiness.dk>
2024-11-14 10:17:44 +0100

    Refactor: Moved inline styles to styled-components and updated global styling

--- Connectly/src/App.jsx
+++ Connectly/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './auth/login';
+import Register from './auth/register';
+import Home from './layout/Home';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
