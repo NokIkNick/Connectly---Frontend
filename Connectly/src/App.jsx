c4c444693c18ca4f4171518909ad673e83135dfd
chris4567890 <starchristian4@hotmail.com>
2024-11-21 13:56:35 +0100

    made posts showable
    made posts showable via the PostList component. have made a public folder for testData.json as it would otherwise not be loaded into the website. styling is incomplete (0 styling have been done) as their was a focus on the functionality rather than tne look.

--- Connectly/src/App.jsx
+++ Connectly/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Post from "./components/Post.jsx";
 import { useEffect, useState } from "react";
+import SpecificFeed from "./page/SpecificFeed.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -10,7 +12,14 @@ function App() {
 
   return (
       <>
-        
+          <div>
+              <BrowserRouter>
+                  <Routes>
+                     <Route path="/test" element={<SpecificFeed/>}/>
+                  </Routes>
+              </BrowserRouter>
+          </div>
+
       </>
   );
 }
---
+++ Connectly/src/components/Posts.jsx
@@ -0,0 +1,12 @@
+export default function Posts({posts}){
+    return(
+        <ul>
+            {posts.map(post)}
+            <li>
+               <h3>{post.title}</h3>
+                <p><strong>Date Created:</strong>{post.date_created}</p>
+                <p>{post.content}</p>
+            </li>
+        </ul>
+    );
+}
