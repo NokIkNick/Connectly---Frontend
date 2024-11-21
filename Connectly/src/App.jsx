8e3e9732510705cb15305aeba5e4a6edf541cd58
Anders W. Jensen <Anders.wade.jensen@hotmail.com>
2024-11-21 14:40:05 +0100

    Update App.jsx

--- Connectly/src/App.jsx
+++ Connectly/src/App.jsx
@@ -1,29 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import React from 'react'
-import { Searchsite } from './page/searchpage'
 
 function App() {
   return (
 
       <>
-          <div>
-              <BrowserRouter>
-                  <Routes>
-                     <Route path="/test" element={<SpecificFeed/>}/>
-                  </Routes>
-              </BrowserRouter>
-          </div>
-
-      </>
-  );
-    <>
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/search" element={<Searchsite/>} />
-        </Routes> 
-      </BrowserRouter>
-
+        
     </>
   
 }
